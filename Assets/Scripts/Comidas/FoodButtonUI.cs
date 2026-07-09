66c7971af5ca3c785e301cacd680d465f8823bfe
AznarWolfpelt <Aznar_Wolfpelt@outlook.com>
2026-07-08 19:41:28 -0500

    Actualizaciones interfaz

---
+++ Assets/Scripts/Comidas/FoodButtonUI.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodButtonUI : MonoBehaviour
+{
+    [Header("UI")]
+    public TMP_Text foodNameText;
+    public Image foodImage;
+    public Button button;
+
+    private FoodData foodData;
+    private FoodManager foodManager;
+
+    public void Setup(FoodData data, FoodManager manager)
+    {
+        foodData = data;
+        foodManager = manager;
+
+        foodNameText.text = data.foodName;
+        foodImage.sprite = data.foodImage;
+
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(OnButtonClicked);
+    }
+
+    void OnButtonClicked()
+    {
+        foodManager.SelectFood(foodData);
+    }
+}
--- Assets/Scripts/Comidas/FoodData.cs
+++ Assets/Scripts/Comidas/FoodData.cs
@@ -10,6 +10,20 @@ public class FoodData : ScriptableObject
     public string foodName;
     public Sprite foodImage;
 
+    [Header("Categoría")]
+    public FoodCategory category;
+
+    public enum FoodCategory
+    {
+        Cereales,
+        Legumbres,
+        Verduras,
+        Frutas,
+        Proteinas,
+        Lacteos,
+        Otros
+    }
+
     [TextArea]
     public string[] portionInfo = new string[4];
 
@@ -25,4 +39,5 @@ public class FoodData : ScriptableObject
 
     [Header("Food Models")]
     public string[] portionObjectNames = new string[4];
+    
 }
